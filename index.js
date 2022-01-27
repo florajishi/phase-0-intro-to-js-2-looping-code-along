48639eb28b10b7410677fde47433c1659f561baa
Jacey Arianna Lowry <jacey.arianna@gmail.com>
2022-01-26 23:51:29 -0600

    Cleaned up code for practice

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
-const names = [ "Guadalupe", "Ollie", "Aki" ];
-let surprise = "surprise";
+const names = ["Guadalupe" , "Ollie" , "Aki"]
+const gifts = ["teddy bear", "drone", "doll"];
 
-function writeCards(names, surprise) {
-    let cards = [];
-    for (let i = 0; i < names.length; i++) {
-        cards.push(`Thank you, ${names[i]}, for the wonderful ${surprise} gift!`)
-    }
-
-    return cards;
+function wrapGifts(gifts) {
+  for(let i = 0; i < gifts.length; i++) {
+    debugger;
+  }
+  return gifts;
 }
-
-function countDown(num) {
-    while (num >= 0) {
-        console.log(num--);
-    }
+function writeCards(names, event) {
+  let cards = [] //empty array
+  for(let i = 0; i < names.length; i++) {
+  cards.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+  }
+  console.log(cards)
+  return cards;
 }
+writeCards(names, "surprise")
 
-writeCards(names, surprise);
-//countDown(10);
+function countDown(i) {
+  while(i>=0)
+  console.log(i--)
+}
+countDown(4);
